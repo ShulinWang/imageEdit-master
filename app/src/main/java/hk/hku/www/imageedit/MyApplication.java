870ee6aad8b405cfb83d4deaca8a42dd91f56202
ShulinWang <63526814+ShulinWang@users.noreply.github.com>
2020-08-01 21:19:01 +0800

    Update MyApplication.java

--- app/src/main/java/hk/hku/www/imageedit/MyApplication.java
+++ app/src/main/java/hk/hku/www/imageedit/MyApplication.java
@@ -1,7 +1,8 @@
 package hk.hku.www.imageedit;
 
 /*
-@author https://github.com/pengjianbo/GalleryFinal
+ * Author:pengjianbo 
+ * URL: https://github.com/pengjianbo/GalleryFinal
  */
 import android.app.Application;
 import android.content.Context;
