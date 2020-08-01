0b85ff3da94f40ac8d77114ebf17fb1018fc96bd
ShulinWang <63526814+ShulinWang@users.noreply.github.com>
2020-08-01 21:18:30 +0800

    Update ImageUploadingActivity.java

--- app/src/main/java/hk/hku/www/imageedit/ImageUploadingActivity.java
+++ app/src/main/java/hk/hku/www/imageedit/ImageUploadingActivity.java
@@ -1,6 +1,7 @@
 package hk.hku.www.imageedit;
 /*
-@author https://github.com/pengjianbo/GalleryFinal
+ * Author:pengjianbo 
+ * URL: https://github.com/pengjianbo/GalleryFinal
  */
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
